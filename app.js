52b1f4e5411bbe884e7f6f92fdad2bfba51c6bc9
ANtibushitse <ntibushitse.arsene@gmail.com>
2016-10-06 16:39:25 +0200

    lastr commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-	var nbClick = 0;
-	$("img").click(function () {
-		console.log("lilu");
-		$("span").text(++nbClick);
-		if (nbClick % 20 === 0) {
-			alert("trouve une activité plus productive");
-		}
-	})
-});
+			var nbClick = 0;
+			$("img").click(function () {
+					console.log("lilu");
+					$("span").text(++nbClick);
+					if (nbClick % 20 === 0) {
+						alert(pseudoAleat);
+						var foo = ["deadpool ", "LukasCage", "Triceratops", "KyloRen"];
+					})
+			});
