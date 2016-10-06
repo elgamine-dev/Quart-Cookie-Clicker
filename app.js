6bdff8bad198f86196cb81762b5f08d953a1647d
ANtibushitse <ntibushitse.arsene@gmail.com>
2016-10-06 16:03:54 +0200

    1er partie done

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
-	//votre code
-	// etape 1  j'ai  créer une variable à incrémenter 
-	var cookie_miam_miam = 0;
+	var nbClick = 0;
 	$("img").click(function () {
-			console.log("jojo");
-			$("span").text(++cookie_miam_miam);
-		})
-		// etape 2  je décide par la suite de  sélectioner la class cookie pour y apposer une fonction click
-		// je décide maintenant  d'ajouter un condition if 
-	if (cookie_miam_miam % 20 === 0) {
-		alert("trouve une activité plus productive");
-	}
+		console.log("lilu");
+		$("span").text(++nbClick);
+		if (nbClick % 20 === 0) {
+			alert("trouve une activité plus productive");
+		}
+	})
 });
