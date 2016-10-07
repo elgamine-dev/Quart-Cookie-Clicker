abc5c6ce317b9e458bd89e314c1d252a41db0832
Amine EL G <aelghayate@simplon.co>
2016-10-07 10:36:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-			var nbClick = 0;
-			$("img").click(function () {
-					console.log("lilu");
-					$("span").text(++nbClick);
-					if (nbClick % 20 === 0) {
-						alert(pseudoAleat);
-						var foo = ["deadpool ", "LukasCage", "Triceratops", "KyloRen"];
-					})
-			});
+	var nbClick = 0;
+	var foo = ["deadpool ", "LukasCage", "Triceratops", "KyloRen"];
+	$("img").click(function () {
+		console.log("lilu");
+		$("span").text(++nbClick);
+		if (nbClick % 20 === 0) {
+			alert(pseudoAleat);
+		}
+
+	});
+});
